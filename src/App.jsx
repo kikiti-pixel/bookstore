150e6dbc2d5ee280a5a84fb48d8dc25e703195af
Kirsi <kirsi.rajaniemi@metropolia.fi>
2026-01-08 16:28:18 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,104 @@
+import { useState, useEffect } from "react";
+import { AgGridReact } from "ag-grid-react";
+import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
+
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+import AddBook from "./AddBook";
+import "./App.css";
+
+ModuleRegistry.registerModules([AllCommunityModule]);
+
+function App() {
+  const [books, setBooks] = useState([]);
+
+  const baseUrl = "https://bookstore-c3f92-default-rtdb.europe-west1.firebasedatabase.app/";
+
+  const [colDefs, setColDefs] = useState([
+    { field: "title", sortable: true, filter: true },
+    { field: "author", sortable: true, filter: true },
+    { field: "year", sortable: true, filter: true },
+    { field: "isbn", sortable: true, filter: true },
+    { field: "price", sortable: true, filter: true },
+    {
+      headerName: "",
+      field: "id",
+      width: 90,
+      cellRenderer: (params) => (
+        <IconButton
+          onClick={() => deleteBook(params.value)}
+          size="small"
+          color="error"
+        >
+          <DeleteIcon />
+        </IconButton>
+      ),
+    },
+  ]);
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const fetchBooks = () => {
+    fetch(`${baseUrl}/books.json`)
+      .then((response) => response.json())
+      .then((data) => {
+
+        if (data === null) {
+          setBooks([]);
+          return;
+        }
+        addKeys(data);
+      })
+      .catch((err) => console.error(err));
+  };
+
+
+  const addKeys = (data) => {
+    const keys = Object.keys(data);
+    const valueKeys = Object.values(data).map((item, index) =>
+      Object.defineProperty(item, "id", { value: keys[index] })
+    );
+    setBooks(valueKeys);
+  };
+
+  const addBook = (newBook) => {
+    fetch(`${baseUrl}/books.json`, {
+      method: "POST",
+      body: JSON.stringify(newBook),
+    })
+      .then(() => fetchBooks())
+      .catch((err) => console.error(err));
+  };
+
+  const deleteBook = (id) => {
+    fetch(`${baseUrl}/books/${id}.json`, {
+      method: "DELETE",
+    })
+      .then(() => fetchBooks())
+      .catch((err) => console.error(err));
+  };
+
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h5">Bookstore</Typography>
+        </Toolbar>
+      </AppBar>
+
+      <AddBook addBook={addBook} />
+
+      <div style={{ height: 500, width: 1200 }}>
+        <AgGridReact rowData={books} columnDefs={colDefs} />
+      </div>
+    </>
+  );
+}
+
+export default App;
